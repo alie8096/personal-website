9c48a83a8f2b4b5cbed84c19ebeb84d21afeeeb1
Ali Ebrahimian <ali.e.8096@outlook.com>
2024-04-07 00:27:35 +0330

    Create and design 404 page

--- apps/blog/routes.py
+++ apps/blog/routes.py
@@ -198,3 +198,6 @@ def admin_profile():
         return render_template("admin_profile.html", form=form)
     return render_template("admin_profile.html", form=EditProfileForm(obj=user))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
